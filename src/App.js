f50601cfb2477ee8c399fb197be953962d9dced3
01edie <01bbose@gmail.com>
2022-07-20 13:43:33 +0530

    react tour app

--- src/App.js
+++ src/App.js
@@ -5,42 +5,42 @@ import './App.css';
 
 const App = () => {
   const [isLoading, setIsLoading] = React.useState(false);
-  const tourUrl= 'https://course-api.com/react-tours-project';
+  const tourUrl = 'https://course-api.com/react-tours-project';
   const [tours, setTours] = React.useState([]);
 
- const deleteTour = (id)=>{
-  let newTours = tours.filter((tour)=>tour.id !== id);
-  setTours(newTours);
+  const deleteTour = (id) => {
+    let newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
 
- }
+  }
 
-  const fetchTour = async () =>{
-    try{
+  const fetchTour = async () => {
+    try {
       setIsLoading(true);
       const response = await fetch(tourUrl);
       const result = await response.json();
       setTours(result);
       setIsLoading(false);
       console.log('fetched');
-    }catch(error){
+    } catch (error) {
       console.log('something went wrong');
     }
   }
-  
-  React.useEffect(()=>{
-    
+
+  React.useEffect(() => {
+
     fetchTour();
-    
-  },[])
-  if(isLoading){
-    return(
+
+  }, [])
+  if (isLoading) {
+    return (
       <Loading></Loading>
     )
   }
   return (
     <>
-    <h2 className='title-heading'>Available Tours Here:</h2>
-    <Tours tours={tours} deleteTour={deleteTour} refresh={fetchTour}></Tours>
+      <h2 className='title-heading'>Available Tours Here:</h2>
+      <Tours tours={tours} deleteTour={deleteTour} refresh={fetchTour}></Tours>
     </>
   )
 }
